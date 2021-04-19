e7b75ec53332e18962a0634d6923c084cff3799c
m0n411111 <bjn.ant1@gmail.com>
2021-04-19 15:25:59 +0200

    finished

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,13 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(word, array)
-
+def substrings(string, dictionary)
+    string.downcase!
+    dictionary.reduce(Hash.new(0)) do |num, word|
+        result = string.scan(word).length
+        num[word] = result if result > 0
+        num
+    end
 end
+
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
