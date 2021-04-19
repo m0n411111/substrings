7b0a2f44c84c941168433317fa2a5987a5351eeb
m0n411111 <bjn.ant1@gmail.com>
2021-04-19 15:29:01 +0200

    added readme

--- substrings.rb
+++ substrings.rb
@@ -10,4 +10,7 @@ def substrings(string, dictionary)
 end
 
 substrings("below", dictionary)
+# => {"below"=>1, "low"=>1}
+
 substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => {"down"=>1, "go"=>1, "going"=>1, "how"=>2, "howdy"=>1, "it"=>2, "i"=>3, "own"=>1, "part"=>1, "partner"=>1, "sit"=>1}
